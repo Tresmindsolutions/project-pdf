74d430da61c74682ac0582f2967cf1c9d332b47c
afham-source <afhamahmed21@gmail.com>
2020-04-09 01:20:18 +0500

    Updates

---
+++ precache-manifest.2ad581d81a054b9ddc738f66182eba5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b408a96b6433fd9dc3aa0f2cf8c12e",
+    "url": "/project-pdf/index.html"
+  },
+  {
+    "revision": "8e4f0be72e54212befa4",
+    "url": "/project-pdf/static/css/main.ee5a3b98.chunk.css"
+  },
+  {
+    "revision": "13f4a741bf53c4a0ffbd",
+    "url": "/project-pdf/static/js/2.99bb216d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-pdf/static/js/2.99bb216d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4f0be72e54212befa4",
+    "url": "/project-pdf/static/js/main.809f056b.chunk.js"
+  },
+  {
+    "revision": "27d075b70bc30bcb63c7",
+    "url": "/project-pdf/static/js/runtime-main.47534a1d.js"
+  },
+  {
+    "revision": "85e3231936e726dd17f007d3a62ba8bd",
+    "url": "/project-pdf/static/media/first.85e32319.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-pdf/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ae54154b6068fab85ae8add45997c0de",
+    "url": "/project-pdf/static/media/second.ae54154b.jpg"
+  },
+  {
+    "revision": "79917209a801d91c9a31548bb24e4eeb",
+    "url": "/project-pdf/static/media/third.79917209.jpg"
+  }
+]);
